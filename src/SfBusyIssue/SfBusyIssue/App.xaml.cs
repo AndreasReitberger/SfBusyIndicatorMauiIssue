853c7a6153641f3103921445b4997c2a69d305d6
Andreas <andy.reitberger@gmail.com>
2024-04-15 09:55:10 +0200

    Added repro

---
+++ src/SfBusyIssue/SfBusyIssue/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SfBusyIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("");
+
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/SfBusyIssue/SfBusyIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SfBusyIssue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
